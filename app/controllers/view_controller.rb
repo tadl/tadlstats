6ba7ef032fde7dffcfbaf5c2b1abe9923344b1be
William Rockwood <wrockwood@tadl.org>
2018-05-09 17:26:36 -0400

    more fleshing
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -25,47 +25,47 @@ class ViewController < ApplicationController
 
         end
 
-        @graph1_data = {
+        @circ_graph = {
             labels: @circ_hash['circdates'],
             datasets: [
                 {
                     label: "Traverse City",
-                    backgroundColor: "rgba(57,106,177,0.2)",
+                    backgroundColor: "rgba(57,106,177,0.5)",
                     borderColor: "rgba(57,106,177,1)",
                     data: @circ_hash['TADL-WOOD'],
                     fill: 'origin',
                 },
                 {
                     label: "East Bay",
-                    backgroundColor: "rgba(218,124,48,0.2)",
+                    backgroundColor: "rgba(218,124,48,0.5)",
                     borderColor: "rgba(218,124,48,1)",
                     data: @circ_hash['TADL-EBB'],
                     fill: '-1',
                 },
                 {
                     label: "Fife Lake",
-                    backgroundColor: "rgba(62,150,81,0.2)",
+                    backgroundColor: "rgba(62,150,81,0.5)",
                     borderColor: "rgba(62,150,81,1)",
                     data: @circ_hash['TADL-FLPL'],
                     fill: '-1',
                 },
                 {
                     label: "Kingsley",
-                    backgroundColor: "rgba(204,37,41,0.2)",
+                    backgroundColor: "rgba(204,37,41,0.5)",
                     borderColor: "rgba(204,37,41,1)",
                     data: @circ_hash['TADL-KBL'],
                     fill: '-1',
                 },
                 {
                     label: "Interlochen",
-                    backgroundColor: "rgba(83,81,84,0.2)",
+                    backgroundColor: "rgba(83,81,84,0.5)",
                     borderColor: "rgba(83,81,84,1)",
                     data: @circ_hash['TADL-IPL'],
                     fill: '-1',
                 },
                 {
                     label: "Peninsula",
-                    backgroundColor: "rgba(107,76,154,0.2)",
+                    backgroundColor: "rgba(107,76,154,0.5)",
                     borderColor: "rgba(107,76,154,1)",
                     data: @circ_hash['TADL-PCL'],
                     fill: '-1',
@@ -89,6 +89,64 @@ class ViewController < ApplicationController
             tooltips: {
                 mode: 'x'
             }
+        }
+
+        @circ_type_graph = {
+            labels: ["Books", "Children's Books", "Music", "Movies", "Other"],
+            datasets: [{
+                data: [32, 22, 7, 27, 12],
+                label: "Percent",
+                backgroundColor: [
+                    "rgba(57,106,177,0.5)",
+                    "rgba(218,124,48,0.5)",
+                    "rgba(62,150,81,0.5)",
+                    "rgba(204,37,41,0.5)",
+                    "rgba(107,76,154,0.5)"
+                ],
+                borderColor: [
+                    "rgba(57,106,177,1)",
+                    "rgba(218,124,48,1)",
+                    "rgba(62,150,81,1)",
+                    "rgba(204,37,41,1)",
+                    "rgba(107,76,154,1)"
+                ],
+                hoverBackgroundColor: [
+                    "rgba(57,106,177,1)",
+                    "rgba(218,124,48,1)",
+                    "rgba(62,150,81,1)",
+                    "rgba(204,37,41,1)",
+                    "rgba(107,76,154,1)"
+                ]
+            }]
+        }
+
+        @collection_dist_graph = {
+            labels: ["Books", "Children's Books", "Music", "Movies", "Other"],
+            datasets: [{
+                data: [44, 23, 11, 8, 14],
+                label: "Percent",
+                backgroundColor: [
+                    "rgba(57,106,177,0.5)",
+                    "rgba(218,124,48,0.5)",
+                    "rgba(62,150,81,0.5)",
+                    "rgba(204,37,41,0.5)",
+                    "rgba(107,76,154,0.5)"
+                ],
+                borderColor: [
+                    "rgba(57,106,177,1)",
+                    "rgba(218,124,48,1)",
+                    "rgba(62,150,81,1)",
+                    "rgba(204,37,41,1)",
+                    "rgba(107,76,154,1)"
+                ],
+                hoverBackgroundColor: [
+                    "rgba(57,106,177,1)",
+                    "rgba(218,124,48,1)",
+                    "rgba(62,150,81,1)",
+                    "rgba(204,37,41,1)",
+                    "rgba(107,76,154,1)"
+                ]
+            }]
         }
 
     end
