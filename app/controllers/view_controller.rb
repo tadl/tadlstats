2c8b4865957aed388f42c9219cbf14ee5a7827d7
William Rockwood <wrockwood@tadl.org>
2018-05-09 12:42:39 -0400

    more tweaking circ hash
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -6,70 +6,68 @@ class ViewController < ApplicationController
         url = "https://www.tadl.org/stats/data/circ-weekly.csv"
         circ_weekly = open(url).read()
         csv = CSV.parse(circ_weekly, :headers => false)
-        @multi = Hash.new()
-        @multi['TADL-WOOD'] = Array.new()
-        @multi['TADL-EBB'] = Array.new()
-        @multi['TADL-FLPL'] = Array.new()
-        @multi['TADL-KBL'] = Array.new()
-        @multi['TADL-IPL'] = Array.new()
-        @multi['TADL-PCL'] = Array.new()
-        @circ_label = []
+        @circ_hash = Hash.new
+        @circ_hash['TADL-WOOD'] = Array.new
+        @circ_hash['TADL-EBB'] = Array.new
+        @circ_hash['TADL-FLPL'] = Array.new
+        @circ_hash['TADL-KBL'] = Array.new
+        @circ_hash['TADL-IPL'] = Array.new
+        @circ_hash['TADL-PCL'] = Array.new
+        @circ_hash['circdates'] = Array.new
         csv.each do |row|
             location, date, count = row
 
             if location == "TADL-WOOD"
-                @circ_label.push(date)
+                @circ_hash['circdates'].push(date)
             end
 
-            @multi[location].push(count)
+            @circ_hash[location].push(count)
 
         end
 
-        puts @multi.inspect
-
         @graph1_data = {
-            labels: @circ_label,
+            labels: @circ_hash['circdates'],
             datasets: [
                 {
                     label: "Traverse City",
                     backgroundColor: "rgba(57,106,177,0.2)",
                     borderColor: "rgba(57,106,177,1)",
-                    data: @multi['TADL-WOOD'],
+                    data: @circ_hash['TADL-WOOD'],
                     fill: 'origin',
                 },
                 {
                     label: "East Bay",
                     backgroundColor: "rgba(218,124,48,0.2)",
                     borderColor: "rgba(218,124,48,1)",
-                    data: @multi['TADL-EBB'],
+                    data: @circ_hash['TADL-EBB'],
                     fill: '-1',
                 },
                 {
                     label: "Fife Lake",
                     backgroundColor: "rgba(62,150,81,0.2)",
                     borderColor: "rgba(62,150,81,1)",
-                    data: @multi['TADL-FLPL'],
+                    data: @circ_hash['TADL-FLPL'],
                     fill: '-1',
                 },
                 {
                     label: "Kingsley",
                     backgroundColor: "rgba(204,37,41,0.2)",
                     borderColor: "rgba(204,37,41,1)",
-                    data: @multi['TADL-KBL'],
+                    data: @circ_hash['TADL-KBL'],
                     fill: '-1',
                 },
                 {
                     label: "Interlochen",
                     backgroundColor: "rgba(83,81,84,0.2)",
                     borderColor: "rgba(83,81,84,1)",
-                    data: @multi['TADL-IPL'],
+                    data: @circ_hash['TADL-IPL'],
                     fill: '-1',
                 },
                 {
                     label: "Peninsula",
                     backgroundColor: "rgba(107,76,154,0.2)",
                     borderColor: "rgba(107,76,154,1)",
-                    data: @multi['TADL-PCL'],
+                    data: @circ_hash['TADL-PCL'],
                     fill: '-1',
                 },
             ]
