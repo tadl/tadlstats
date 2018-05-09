d2666e6a7d2098015ed4e8a065f7c8637203dabf
William Rockwood <wrockwood@tadl.org>
2018-05-09 10:19:38 -0400

    circ graph functional
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -6,69 +6,89 @@ class ViewController < ApplicationController
         url = "https://www.tadl.org/stats/data/circ-weekly.csv"
         circ_weekly = open(url).read()
         csv = CSV.parse(circ_weekly, :headers => false)
-        @location_hash = {}
-        #@temp_hash = {}
+        @circ_label = []
+        @circ_tc = []
+        @circ_ebb = []
+        @circ_flpl = []
+        @circ_kbl = []
+        @circ_ipl = []
+        @circ_pcl = []
         csv.each do |row|
-            # puts "row: " + row.to_s
-            # row: ["TADL-EBB", "2018-04-30", "629"]
             location, date, count = row
-            #@temp_hash = { date: count }
-            #@location_hash[location].merge!(AARGGGHGHHHHH
+
+            if location == "TADL-WOOD"
+                @circ_label.push(date)
+                @circ_tc.push(count)
+            end
+            if location == "TADL-EBB"
+                @circ_ebb.push(count)
+            end
+            if location == "TADL-FLPL"
+                @circ_flpl.push(count)
+            end
+            if location == "TADL-KBL"
+                @circ_kbl.push(count)
+            end
+            if location == "TADL-IPL"
+                @circ_ipl.push(count)
+            end
+            if location == "TADL-PCL"
+                @circ_pcl.push(count)
+            end
+
         end
 
-        #puts @location_hash.inspect
-            
         @graph1_data = {
-            labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            labels: @circ_label,
             datasets: [
                 {
                     label: "Traverse City",
                     backgroundColor: "rgba(57,106,177,0.2)",
                     borderColor: "rgba(57,106,177,1)",
-                    data: [81, 95, 74, 79, 85, 88, 97, 83, 90, 77, 68, 83],
+                    data: @circ_tc,
                     fill: 'origin',
                 },
                 {
                     label: "East Bay",
                     backgroundColor: "rgba(218,124,48,0.2)",
                     borderColor: "rgba(218,124,48,1)",
-                    data: [14, 18, 21, 17, 23, 24, 29, 19, 22, 11, 9, 16],
+                    data: @circ_ebb,
                     fill: '-1',
                 },
                 {
                     label: "Fife Lake",
                     backgroundColor: "rgba(62,150,81,0.2)",
                     borderColor: "rgba(62,150,81,1)",
-                    data: [12, 28, 28, 12, 20, 21, 22, 29, 13, 17, 12, 10],
+                    data: @circ_flpl,
                     fill: '-1',
                 },
                 {
                     label: "Kingsley",
                     backgroundColor: "rgba(204,37,41,0.2)",
                     borderColor: "rgba(204,37,41,1)",
-                    data: [24, 28, 21, 27, 33, 34, 39, 29, 32, 21, 19, 26],
+                    data: @circ_kbl,
                     fill: '-1',
                 },
                 {
                     label: "Interlochen",
                     backgroundColor: "rgba(83,81,84,0.2)",
                     borderColor: "rgba(83,81,84,1)",
-                    data: [14, 18, 21, 17, 23, 24, 29, 19, 22, 11, 9, 16],
+                    data: @circ_ipl,
                     fill: '-1',
                 },
                 {
                     label: "Peninsula",
                     backgroundColor: "rgba(107,76,154,0.2)",
                     borderColor: "rgba(107,76,154,1)",
-                    data: [14, 18, 21, 17, 23, 24, 29, 19, 22, 11, 9, 16],
+                    data: @circ_pcl,
                     fill: '-1',
                 },
             ]
         }
 
         @line_graph_options = {
-            width: 1280,
-            height: 720,
+            width: 1200,
+            height: 480,
             plugins: {
                 filler: {
                     propogate: true
