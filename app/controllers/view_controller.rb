741e755b9dd98e0d750eb13c5716028859f43c8f
William Rockwood <wrockwood@tadl.org>
2018-05-09 12:17:40 -0400

    clean up csv parsing using a hash instead of arrays
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -6,37 +6,26 @@ class ViewController < ApplicationController
         url = "https://www.tadl.org/stats/data/circ-weekly.csv"
         circ_weekly = open(url).read()
         csv = CSV.parse(circ_weekly, :headers => false)
+        @multi = Hash.new()
+        @multi['TADL-WOOD'] = Array.new()
+        @multi['TADL-EBB'] = Array.new()
+        @multi['TADL-FLPL'] = Array.new()
+        @multi['TADL-KBL'] = Array.new()
+        @multi['TADL-IPL'] = Array.new()
+        @multi['TADL-PCL'] = Array.new()
         @circ_label = []
-        @circ_tc = []
-        @circ_ebb = []
-        @circ_flpl = []
-        @circ_kbl = []
-        @circ_ipl = []
-        @circ_pcl = []
         csv.each do |row|
             location, date, count = row
 
             if location == "TADL-WOOD"
                 @circ_label.push(date)
-                @circ_tc.push(count)
-            end
-            if location == "TADL-EBB"
-                @circ_ebb.push(count)
-            end
-            if location == "TADL-FLPL"
-                @circ_flpl.push(count)
-            end
-            if location == "TADL-KBL"
-                @circ_kbl.push(count)
-            end
-            if location == "TADL-IPL"
-                @circ_ipl.push(count)
-            end
-            if location == "TADL-PCL"
-                @circ_pcl.push(count)
             end
 
+            @multi[location].push(count)
+
         end
+
+        puts @multi.inspect
 
         @graph1_data = {
             labels: @circ_label,
@@ -45,42 +34,42 @@ class ViewController < ApplicationController
                     label: "Traverse City",
                     backgroundColor: "rgba(57,106,177,0.2)",
                     borderColor: "rgba(57,106,177,1)",
-                    data: @circ_tc,
+                    data: @multi['TADL-WOOD'],
                     fill: 'origin',
                 },
                 {
                     label: "East Bay",
                     backgroundColor: "rgba(218,124,48,0.2)",
                     borderColor: "rgba(218,124,48,1)",
-                    data: @circ_ebb,
+                    data: @multi['TADL-EBB'],
                     fill: '-1',
                 },
                 {
                     label: "Fife Lake",
                     backgroundColor: "rgba(62,150,81,0.2)",
                     borderColor: "rgba(62,150,81,1)",
-                    data: @circ_flpl,
+                    data: @multi['TADL-FLPL'],
                     fill: '-1',
                 },
                 {
                     label: "Kingsley",
                     backgroundColor: "rgba(204,37,41,0.2)",
                     borderColor: "rgba(204,37,41,1)",
-                    data: @circ_kbl,
+                    data: @multi['TADL-KBL'],
                     fill: '-1',
                 },
                 {
                     label: "Interlochen",
                     backgroundColor: "rgba(83,81,84,0.2)",
                     borderColor: "rgba(83,81,84,1)",
-                    data: @circ_ipl,
+                    data: @multi['TADL-IPL'],
                     fill: '-1',
                 },
                 {
                     label: "Peninsula",
                     backgroundColor: "rgba(107,76,154,0.2)",
                     borderColor: "rgba(107,76,154,1)",
-                    data: @circ_pcl,
+                    data: @multi['TADL-PCL'],
                     fill: '-1',
                 },
             ]
