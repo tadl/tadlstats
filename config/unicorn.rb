6ad08a83bdd8a9f06ac33652c05508ac57728f3e
William Rockwood <wrockwood@tadl.org>
2018-05-10 11:03:45 -0400

    try try try
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,8 +8,6 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
@@ -17,6 +15,4 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
 end
