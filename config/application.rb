d20254003cb66d93f7604e425f6cfe5316ebb8ad
William Rockwood <wrockwood@tadl.org>
2018-05-12 16:57:03 -0400

    configure config/dotenv
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- config/application.rb
+++ config/application.rb
@@ -19,12 +19,8 @@ Bundler.require(*Rails.groups)
 
 module Tadlstats
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    Settings.add_source!("#{Rails.root}/config/settings/" + ENV["SYSTEM"] + ".yml")
   end
 end
