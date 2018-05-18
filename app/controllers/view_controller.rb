71e723a7e107f3cc88414397f59d9542d8ce3f9f
William Rockwood <wrockwood@tadl.org>
2018-05-18 13:48:53 -0400

    finish up everything but wireless
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -13,13 +13,10 @@ class ViewController < ApplicationController
         @pubcomp_hash = Rails.cache.read('pubcomp_weekly')
         @newusers_hash = Rails.cache.read('newusers_weekly')
 
-        # Page title in header
-        @longname = "All Locations"
-
         # Color definitions for pie/circle graphs
-        pie_chart_colors = [ "rgba(57,106,177,1)", "rgba(218,124,48,1)", "rgba(62,150,81,1)", "rgba(204,37,41,1)",
+        chart_colors = [ "rgba(57,106,177,1)", "rgba(218,124,48,1)", "rgba(62,150,81,1)", "rgba(204,37,41,1)",
                              "rgba(107,76,154,1)", "rgba(83,81,84,1)", "rgba(146,36,40,1)", "rgba(148,139,61,1)" ]
-        pie_chart_halftones = [ "rgba(57,106,177,0.5)", "rgba(218,124,48,0.5)", "rgba(62,150,81,0.5)", "rgba(204,37,41,0.5)",
+        chart_halftones = [ "rgba(57,106,177,0.5)", "rgba(218,124,48,0.5)", "rgba(62,150,81,0.5)", "rgba(204,37,41,0.5)",
                                 "rgba(107,76,154,0.5)", "rgba(83,81,84,0.5)", "rgba(146,36,40,0.5)", "rgba(148,139,61,0.5)" ]
 
         # Collection Size (box + graph)
@@ -64,6 +61,23 @@ class ViewController < ApplicationController
 
         end
 
+        @circ_type_graph = {
+            labels: stats_circ_by_type_graph_labels,
+            datasets: [{
+                data: stats_circ_by_type_graph_data,
+                label: "Circulations",
+                backgroundColor: chart_halftones,
+                borderColor: chart_colors,
+                hoverBackgroundColor: chart_colors
+            }]
+        }
+
+        @circ_type_graph_options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: { display: false },
+            animation: { duration: 0 },
+        }
 
         # Computer sessions / users (box)
         @stats_computer_sessions = @stats_data["pubcomp_ytd"]["total"][:sessions]
@@ -124,6 +138,7 @@ class ViewController < ApplicationController
                 xAxes: [{ type: 'time', time: { unit: 'month', displayFormats: { month: 'MMM YY' } }, distribution: 'series' }]
             },
             tooltips: { mode: 'index', axis: 'x', intersect: false },
+            animation: { duration: 0 },
             responsive: true,
             maintainAspectRatio: false
         }
@@ -162,6 +177,7 @@ class ViewController < ApplicationController
                 xAxes: [{ type: 'time', time: { unit: 'month', displayFormats: { month: 'MMM YY' } }, distribution: 'series' }]
             },
             tooltips: { mode: 'index', axis: 'x', intersect: false },
+            animation: { duration: 0 },
             responsive: true,
             maintainAspectRatio: false
         }
@@ -200,59 +216,9 @@ class ViewController < ApplicationController
                 xAxes: [{ type: 'time', time: { unit: 'month', displayFormats: { month: 'MMM YY' } }, distribution: 'series' }]
             },
             tooltips: { mode: 'index', axis: 'x', intersect: false },
+            animation: { duration: 0 },
             responsive: true,
             maintainAspectRatio: false
-        }
-
-        # Collection Movement (graph)
-        @stats_collection_movement_labels = Array.new
-        @stats_collection_movement_additions = Array.new
-        @stats_collection_movement_withdrawls = Array.new
-
-        @stats_data["copies_added_ytd"]["total"].each do |key, val|
-            @stats_collection_movement_labels.push(item_type_map(key))
-            @stats_collection_movement_additions.push(val)
-        end
-        @stats_data["copies_withdrawn_ytd"]["total"].each do |key, val|
-            @stats_collection_movement_withdrawls.push(val)
-        end
-
-        @collection_movement_graph = {
-            labels: @stats_collection_movement_labels,
-            datasets: [
-                {
-                    label: "Additions",
-                    data: @stats_collection_movement_additions,
-                    backgroundColor: "rgba(62,150,81,0.5)",
-                    borderColor: "rgba(62,150,81,1)",
-                    pointRadius: 3,
-                    pointHitRadius: 8,
-                    pointHoverRadius: 5,
-                    pointBorderWidth: 1,
-                    pointStyle: 'rectRounded',
-                    fill: false,
-                    radius: 6,
-                },
-                {
-                    label: "Withdrawls",
-                    data: @stats_collection_movement_withdrawls,
-                    backgroundColor: "rgba(204,37,41,0.5)",
-                    borderColor: "rgba(204,37,41,1)",
-                    pointRadius: 3,
-                    pointHitRadius: 8,
-                    pointHoverRadius: 5,
-                    pointBorderWidth: 1,
-                    pointStyle: 'rectRounded',
-                    fill: false,
-                    radius: 6,
-                }
-            ]
-        }
-        @collection_movement_graph_options = {
-            scale: {
-                ticks: { min: 0, max: 5000, stepSize: 1000 }
-            },
-            tooltips: { mode: 'index', axis: 'x', intersect: true, displayColors: false }
         }
 
         # Wireless Sessions (graph) TODO
@@ -339,20 +305,9 @@ class ViewController < ApplicationController
             plugins: { filler: { propogate: true } },
             scales: { yAxes: [{ stacked: true }] },
             tooltips: { mode: 'index', axis: 'x', intersect: false },
+            animation: { duration: 0 },
             responsive: true,
             maintainAspectRatio: false
-        }
-
-        # Circulation by Type (graph)
-        @circ_type_graph = {
-            labels: stats_circ_by_type_graph_labels,
-            datasets: [{
-                data: stats_circ_by_type_graph_data,
-                label: "Percent",
-                backgroundColor: pie_chart_halftones,
-                borderColor: pie_chart_colors,
-                hoverBackgroundColor: pie_chart_colors
-            }]
         }
 
         # Collection Distribution (graph)
@@ -361,9 +316,9 @@ class ViewController < ApplicationController
             datasets: [{
                 data: stats_collection_size_graph_data,
                 label: "Percent",
-                backgroundColor: pie_chart_halftones,
-                borderColor: pie_chart_colors,
-                hoverBackgroundColor: pie_chart_colors
+                backgroundColor: chart_halftones,
+                borderColor: chart_colors,
+                hoverBackgroundColor: chart_colors
             }]
         }
 
@@ -374,7 +329,8 @@ class ViewController < ApplicationController
         Settings.locations.each do |l|
             if l.path_name == params[:location]
                 @loc = l
-                @longname = @loc.long_name
+                @short_name = l.short_name
+                @evergreen_name = l.evergreen_name
                 break
             else
                 @loc["path_name"] = "invalid"
@@ -393,8 +349,213 @@ class ViewController < ApplicationController
         @pubcomp_hash = Rails.cache.read('pubcomp_weekly')
         @newusers_hash = Rails.cache.read('newusers_weekly')
 
+        # Color definitions for pie/circle graphs
+        chart_colors = [ "rgba(57,106,177,1)", "rgba(218,124,48,1)", "rgba(62,150,81,1)", "rgba(204,37,41,1)",
+                             "rgba(107,76,154,1)", "rgba(83,81,84,1)", "rgba(146,36,40,1)", "rgba(148,139,61,1)" ]
+        chart_halftones = [ "rgba(57,106,177,0.5)", "rgba(218,124,48,0.5)", "rgba(62,150,81,0.5)", "rgba(204,37,41,0.5)",
+                                "rgba(107,76,154,0.5)", "rgba(83,81,84,0.5)", "rgba(146,36,40,0.5)", "rgba(148,139,61,0.5)" ]
+
+        # Collection Size (box + graph) -
+        @stats_collection_size = 0
+        stats_collection_size_graph_data = Array.new
+        stats_collection_size_graph_labels = Array.new
+        @stats_collection_size_books = 0
+        @stats_audio_visual_materials = 0
+
+        @stats_data["collection_size"][@evergreen_name].each do |type, val|
+            @stats_collection_size += val.to_i
+
+            if type.include? "book"
+                @stats_collection_size_books += val.to_i
+            end
+
+            if (type.include?("movies") || type.include?("music") || type.include?("audiobooks"))
+                @stats_audio_visual_materials += val.to_i
+            end
+
+            stats_collection_size_graph_data.push(val)
+            stats_collection_size_graph_labels.push(item_type_map(type))
+        end
+
+        # Circulation by Type (graph + box) -
+        stats_circ_by_type_graph_data = Array.new
+        stats_circ_by_type_graph_labels = Array.new
+        @stats_circ_by_type_books = 0
+        @stats_circ_by_type_av = 0
+
+        @stats_data["circ_by_type_ytd"][@evergreen_name].each do |type, val|
+            stats_circ_by_type_graph_data.push(val)
+            stats_circ_by_type_graph_labels.push(item_type_map(type))
+
+            if type.include? "book"
+                @stats_circ_by_type_books += val.to_i
+            end
+
+            if (type.include?("movies") || type.include?("music") || type.include?("audiobooks"))
+                @stats_circ_by_type_av += val.to_i
+            end
+
+        end
+
+        @circ_type_graph = {
+            labels: stats_circ_by_type_graph_labels,
+            datasets: [{
+                data: stats_circ_by_type_graph_data,
+                label: "Circulations",
+                backgroundColor: chart_halftones,
+                borderColor: chart_colors,
+                hoverBackgroundColor: chart_colors
+            }]
+        }
+
+        @circ_type_graph_options = {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: { display: false },
+            animation: { duration: 0 },
+        }
+
+        # Computer sessions / users (box) -
+        @stats_computer_sessions = @stats_data["pubcomp_ytd"][@short_name][:sessions]
+        @stats_computer_users = @stats_data["pubcomp_ytd"][@short_name][:users]
 
 
+        # Items Circulated (box) -
+        @stats_items_circulated = 0
+
+        @stats_data["circ_by_type_ytd"][@evergreen_name].each do |type, val|
+            @stats_items_circulated += val.to_i
+        end
+
+
+        # Collection Stats (table) -
+        @stats_collection_stats = @stats_data["collection_size"]
+        @stats_collection_stats_specific = @stats_data["collection_size"][@evergreen_name]
+        @stats_copies_added = @stats_data["copies_added_ytd"][@evergreen_name]
+        @stats_copies_withdrawn = @stats_data["copies_withdrawn_ytd"][@evergreen_name]
+
+
+        # Weekly Circulation (graph) -
+        @circ_graph = Hash.new
+        @circ_graph[:labels] = @circ_hash['graphdates']
+        @circ_graph[:datasets] = Array.new
+
+        circloc = Hash.new
+
+        circloc = {
+            label: @loc.short_name,
+            backgroundColor: @loc.background_color,
+            borderColor: @loc.border_color,
+            data: @circ_hash[@loc.evergreen_name],
+            fill: "origin",
+            pointRadius: 2,
+            pointHitRadius: 1,
+            pointHoverRadius: 5,
+            pointBorderWidth: 1,
+            pointStyle: 'rectRounded'
+        }
+
+        @circ_graph[:datasets].push(circloc)
+
+        @circ_graph_options = {
+            width: 1200,
+            height: 480,
+            plugins: { filler: { propogate: true } },
+            scales: {
+                yAxes: [{ stacked: true }],
+                xAxes: [{ type: 'time', time: { unit: 'month', displayFormats: { month: 'MMM YY' } }, distribution: 'series' }]
+            },
+            animation: { duration: 0 },
+            tooltips: { mode: 'index', axis: 'x', intersect: false },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+
+        # Public Computer Sessions (graph) -
+        @pubcomp_graph = Hash.new
+        @pubcomp_graph[:labels] = @pubcomp_hash['graphdates']
+        @pubcomp_graph[:datasets] = Array.new
+
+        pubcomploc = Hash.new
+
+        pubcomploc = {
+            label: @loc.short_name,
+            backgroundColor: @loc.background_color,
+            borderColor: @loc.border_color,
+            data: @pubcomp_hash['sessions'][@loc.short_name],
+            fill: "origin",
+            pointRadius: 2,
+            pointHitRadius: 1,
+            pointHoverRadius: 5,
+            pointBorderWidth: 1,
+            pointStyle: 'rectRounded'
+        }
+
+        @pubcomp_graph[:datasets].push(pubcomploc)
+
+        @pubcomp_graph_options = {
+            width: 1200,
+            height: 480,
+            plugins: { filler: { propogate: true } },
+            scales: {
+                yAxes: [{ stacked: true }],
+                xAxes: [{ type: 'time', time: { unit: 'month', displayFormats: { month: 'MMM YY' } }, distribution: 'series' }]
+            },
+            tooltips: { mode: 'index', axis: 'x', intersect: false },
+            animation: { duration: 0 },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+
+        # New Users (graph)
+        @newusers_graph = Hash.new
+        @newusers_graph[:labels] = @newusers_hash['graphdates']
+        @newusers_graph[:datasets] = Array.new
+
+        usersloc = Hash.new
+
+        usersloc = {
+            label: @loc.short_name,
+            backgroundColor: @loc.background_color,
+            borderColor: @loc.border_color,
+            data: @newusers_hash[@loc.evergreen_name],
+            fill: "origin",
+            pointRadius: 2,
+            pointHitRadius: 1,
+            pointHoverRadius: 5,
+            pointBorderWidth: 1,
+            pointStyle: 'rectRounded'
+        }
+
+        @newusers_graph[:datasets].push(usersloc)
+
+        @newusers_graph_options = {
+            width: 1200,
+            height: 480,
+            plugins: { filler: { propogate: true } },
+            scales: {
+                yAxes: [{ stacked: true }],
+                xAxes: [{ type: 'time', time: { unit: 'month', displayFormats: { month: 'MMM YY' } }, distribution: 'series' }]
+            },
+            tooltips: { mode: 'index', axis: 'x', intersect: false },
+            animation: { duration: 0 },
+            responsive: true,
+            maintainAspectRatio: false
+        }
+
+        # Wireless Sessions (graph) TODO -
+
+        # Collection Distribution (graph) -
+        @collection_dist_graph = {
+            labels: stats_collection_size_graph_labels,
+            datasets: [{
+                data: stats_collection_size_graph_data,
+                label: "Percent",
+                backgroundColor: chart_halftones,
+                borderColor: chart_colors,
+                hoverBackgroundColor: chart_colors
+            }]
+        }
 
     end
 
