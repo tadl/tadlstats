7d5761e4bb34f9216c0722de86a9e61a1c477b87
William Rockwood <wrockwood@tadl.org>
2018-05-24 16:39:15 -0400

    add percentages to collection distribution chart
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- app/controllers/view_controller.rb
+++ app/controllers/view_controller.rb
@@ -310,6 +310,25 @@ class ViewController < ApplicationController
       }]
     }
 
+    @collection_dist_graph_options = {
+      tooltips: {
+        callbacks: {
+          label: "function(tooltipItem, data) {
+            var dataset = data.datasets[tooltipItem.datasetIndex];
+            var meta = dataset._meta[Object.keys(dataset._meta)[0]];
+            var total = meta.total;
+            var currentValue = dataset.data[tooltipItem.index];
+            var percentage = parseFloat((currentValue/total*100).toFixed(1));
+            return currentValue + ' (' + percentage + '%)';
+          }",
+          title: "function(tooltipItem, data) {
+            return data.labels[tooltipItem[0].index];
+          }"
+        }
+      }
+    }
+
+
   end
 
   def about
